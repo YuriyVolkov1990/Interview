a13c6109ad366580f32a5cddefff2c87adac359e
Professional <new99kpicem@gmail.com>
2023-09-27 22:39:33 +0300

    task

---
+++ src/Student.java
@@ -0,0 +1,27 @@
+import java.util.Objects;
+
+public class Student {
+    private String name;
+    private int age;
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this==o) return true;
+        if (o == null || getClass()!=o.getClass()) return false;
+        Student student = (Student) o;
+        return age == student.age && name.equals(student.name);
+
+    }
+    @Override
+    public int hashCode() {
+        int t =31;
+        t = t * 31 + name.hashCode();
+        t = t * 31 + age;
+        return t;
+    }
+}
